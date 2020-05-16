7f1ae8adcf414c617969dcb99309bb7765e35bf2
Pekerle <63566621+Pekerle@users.noreply.github.com>
2020-05-15 21:45:54 -0300

    Add files via upload
    Correcciones y recursividad

--- TP_04-05_Pekerle.hs
+++ TP_04-05_Pekerle.hs
@@ -34,10 +34,10 @@ manuel :: Jugador
 manuel = UnJugador "Manuel" 500 "Oferente singular" [] [pasarPorElBanco, enojarse]
 
 modificarNombre :: (Nombre -> Nombre) -> Jugador -> Jugador
-modificarNombre unaFuncion unJugador = unJugador {nombre = unaFuncion(nombre unJugador)}
+modificarNombre unaFuncion unJugador = unJugador {nombre = unaFuncion (nombre unJugador)}
 
 modificarCantidadDeDinero :: (CantidadDeDinero -> CantidadDeDinero) -> Jugador -> Jugador
-modificarCantidadDeDinero unaFuncion unJugador = unJugador {cantidadDeDinero = unaFuncion(cantidadDeDinero unJugador)}
+modificarCantidadDeDinero unaFuncion unJugador = unJugador {cantidadDeDinero = unaFuncion (cantidadDeDinero unJugador)}
 
 cambiarTactica :: Tactica -> Jugador -> Jugador
 cambiarTactica unaTactica unJugador = unJugador {tactica = unaTactica}
@@ -49,7 +49,7 @@ agregarAccion :: Accion -> Jugador -> Jugador
 agregarAccion unaAccion unJugador = unJugador {acciones = acciones unJugador ++ [unaAccion]}
 
 pasarPorElBanco :: Jugador -> Jugador
-pasarPorElBanco unJugador = (modificarCantidadDeDinero (+ 500) . cambiarTactica "Comprador compulsivo" . agregarAccion pasarPorElBanco) unJugador
+pasarPorElBanco unJugador = (modificarCantidadDeDinero (+ 40) . cambiarTactica "Comprador compulsivo" . agregarAccion pasarPorElBanco) unJugador
 
 enojarse :: Jugador -> Jugador
 enojarse unJugador = (modificarCantidadDeDinero (+ 50) . agregarAccion enojarse . agregarAccion gritar) unJugador
@@ -62,8 +62,8 @@ esTacticaBuscada unaTactica = elem unaTactica ["Accionista", "Oferente singular"
 
 subastar :: Propiedad -> Jugador -> Jugador
 subastar unaPropiedad unJugador
-    | (esTacticaBuscada.tactica) unJugador = (modificarCantidadDeDinero (subtract (precioPropiedad unaPropiedad)) . agregarAccion (subastar unaPropiedad) ) unJugador
-    | otherwise = unJugador
+    | (esTacticaBuscada.tactica) unJugador = (modificarCantidadDeDinero (subtract (precioPropiedad unaPropiedad)) . agregarPropiedad (unaPropiedad) . agregarAccion (subastar unaPropiedad) ) unJugador
+    | otherwise                            = unJugador
 
 cobroUnitario :: Propiedad -> Float
 cobroUnitario unaPropiedad
@@ -74,11 +74,26 @@ totalDeAlquileres :: Propiedades -> Float
 totalDeAlquileres unasPropiedades = (sum.map cobroUnitario) unasPropiedades
 
 cobrarAlquileres :: Jugador -> Jugador
-cobrarAlquileres unJugador 
-    | propiedades unJugador == [] = unJugador
-    | otherwise                   = (modificarCantidadDeDinero (+ totalDeAlquileres (propiedades unJugador)) . agregarAccion cobrarAlquileres) unJugador
+cobrarAlquileres (UnJugador unNombre unaCantidadDeDinero unaTactica [] unasAcciones) = UnJugador unNombre unaCantidadDeDinero unaTactica [] unasAcciones
+cobrarAlquileres unJugador                                                           = (modificarCantidadDeDinero (+ totalDeAlquileres (propiedades unJugador)) . agregarAccion cobrarAlquileres) unJugador
 
 pagarAAccionistas :: Jugador -> Jugador
-pagarAAccionistas unJugador
-    | tactica unJugador == "Accionista" = (modificarCantidadDeDinero (+200)         . agregarAccion pagarAAccionistas) unJugador
-    | otherwise                         = (modificarCantidadDeDinero (subtract 100) . agregarAccion pagarAAccionistas) unJugador
+pagarAAccionistas (UnJugador unNombre unaCantidadDeDinero "Accionista" unasPropiedades unasAcciones) = UnJugador unNombre (unaCantidadDeDinero +200) "Accionista" unasPropiedades (unasAcciones ++ [pagarAAccionistas])
+pagarAAccionistas unJugador                                                                          = (modificarCantidadDeDinero (subtract 100) . agregarAccion pagarAAccionistas) unJugador
+
+hacerBerrinchePor :: Propiedad -> Jugador -> Jugador
+hacerBerrinchePor unaPropiedad unJugador 
+    | precioPropiedad unaPropiedad <= cantidadDeDinero unJugador = (modificarCantidadDeDinero (subtract (precioPropiedad unaPropiedad)) . agregarPropiedad unaPropiedad) unJugador
+    | otherwise                                                  = hacerBerrinchePor unaPropiedad ((modificarCantidadDeDinero (+ 10) . gritar . agregarAccion (hacerBerrinchePor unaPropiedad)) unJugador )
+
+ultimaRonda :: Jugador -> Accion
+ultimaRonda unJugador = foldl1 (.) ((reverse.acciones) unJugador)
+
+dineroEnUltimaRonda :: Jugador -> CantidadDeDinero
+dineroEnUltimaRonda unJugador = (cantidadDeDinero.ultimaRonda unJugador) unJugador
+
+juegoFinal :: Jugador -> Jugador -> String
+juegoFinal jugador1 jugador2
+    | dineroEnUltimaRonda jugador1 == dineroEnUltimaRonda jugador2 = "Termino en empate"
+    | dineroEnUltimaRonda jugador1  > dineroEnUltimaRonda jugador2 = "Gano el Jugador 1"
+    | otherwise                                                    = "Gano el Jugador 2"
